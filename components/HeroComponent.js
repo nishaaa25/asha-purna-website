b68c7610001eb6fb897fb1ffffea4fb4bd547eec
nishaaa25 <sahnisha2505@gmail.com>
2025-11-29 16:05:18 +0530

    refactor HeroComponent to replace YouTube video with Vimeo and simplify video handling

--- components/HeroComponent.js
+++ components/HeroComponent.js
@@ -1,59 +1,33 @@
 "use client";
-import { useEffect } from "react";
 import HorizontalForm from "./HorizontalForm";
 
 export default function HeroComponent() {
-  useEffect(() => {
-    const tag = document.createElement("script");
-    tag.src = "https://www.youtube.com/iframe_api";
-    document.body.appendChild(tag);
-
-    let player;
-
-    window.onYouTubeIframeAPIReady = () => {
-      player = new window.YT.Player("bg-video", {
-        events: {
-          onReady: (event) => {
-            event.target.mute();
-            event.target.playVideo();
-
-            // Force highest available quality
-            event.target.setPlaybackQuality("highres");
-
-            // Force again after 2 sec to override YouTube auto-quality
-            setTimeout(() => {
-              event.target.setPlaybackQuality("highres");
-            }, 2000);
-          },
-        },
-      });
-    };
-  }, []);
 
   return (
     <section className="w-full relative h-screen flex-center overflow-hidden">
       {/* Background Video */}
       <div className="absolute top-0 left-0 w-full h-full overflow-hidden -z-10">
-        <div className="absolute top-1/2 left-1/2 w-[400%] h-[400%] md:w-[120%] md:h-[120%] -translate-x-1/2 -translate-y-1/2">
+        <div className="absolute top-1/2 left-1/2 w-full h-full -translate-x-1/2 -translate-y-1/2">
           <iframe
-            id="bg-video"
+            id="bg-video-desktop"
             fetchPriority="high"
-            className="w-full h-full pointer-events-none object-cover"
-            src={`https://www.youtube.com/embed/yMOk_HcPunk?
-              autoplay=1
-              &mute=1
-              &controls=0
-              &modestbranding=1
-              &rel=0
-              &loop=1
-              &playlist=yMOk_HcPunk
-              &playsinline=1
-              &enablejsapi=1
-            `.replace(/\s+/g, "")}
+            className="w-full h-full pointer-events-none object-cover hidden  scale-110  lg:flex"
+            src="https://player.vimeo.com/video/1141020580?autoplay=1&mute=1&controls=0&loop=1&playsinline=1"
             title="Background Video"
             frameBorder="0"
             allow="autoplay; fullscreen; picture-in-picture"
             allowFullScreen
+            playsInline
+          ></iframe>
+          <iframe
+            id="bg-video-mobile"
+            fetchPriority="high"
+            className="w-full h-full pointer-events-none object-cover scale-120 lg:hidden"
+            src="https://player.vimeo.com/video/1141020698?autoplay=1&mute=1&controls=0&loop=1&playsinline=1"
+            title="Background Video"
+            frameBorder="0"
+            allow="autoplay; fullscreen;  picture-in-picture"
+            playsInline
           ></iframe>
         </div>
       </div>
